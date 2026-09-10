8a428457e53676a852bb954e42767aa5ccae372a
PurviBheda <pnbheda924@gmail.com>
2026-09-10 11:17:40 +0530

    if-elif-else

--- task15.py
+++ task15.py
@@ -1,2 +1,15 @@
-# Compare two numbers and print which is larger.
+#Electricity Bill using if, elif, else
+
+units = int(input("Enter your Units: "))
+if units <= 100:
+    print("Low Consumption")
+elif units <= 300:
+    print("Medium Consumption")
+else:
+    print("High Consumption")
+
+
+
+
+
 
