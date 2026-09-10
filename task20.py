04753381f0e23845e30a868ab48fa227d40e9914
PurviBheda <pnbheda924@gmail.com>
2026-09-10 13:24:35 +0530

    Implement login check functionality with username and password validation

--- task20.py
+++ task20.py
@@ -13,3 +13,19 @@ Correct username + correct password → Login successful
 Correct username + wrong password → Wrong password
 Wrong username → Wrong username""""""
 """
+
+
+username = "purvi"
+password = "python123"
+
+e_u = input("Enter Username: ")
+e_p = input("Enter Password: ")
+
+if e_u == username:
+    if e_p == password:
+        print("Login Successful!")
+    else:
+        print("Incorrect Passowrd")
+else:
+    print("Incorrect Username")
+
