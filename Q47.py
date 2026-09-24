4aa8320a3684ea57d649b6f8190631ef193a9b69
PurviBheda <pnbheda924@gmail.com>
2026-09-24 19:56:36 +0530

    Add list comprehensions for new numbers, multiplication, and squares

---
+++ Q47.py
@@ -0,0 +1,13 @@
+
+numbers = [1,2,3,4,5]
+new_numbers = [new_numbers for new_numbers in numbers]
+print(new_numbers)
+
+numbers = [1,2,3,4,5]
+multiply = [multiply * 2 for multiply in numbers]
+print(multiply)
+
+numbers = [1,2,3,4,5]
+sqaures = [sqaures * sqaures for sqaures in numbers]
+print(sqaures)
+
